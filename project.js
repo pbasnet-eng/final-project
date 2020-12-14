5281a4b160b585cc5fd4a6219b1e8c85e0a9dbc9
pbasnet-eng <75640806+pbasnet-eng@users.noreply.github.com>
2020-12-13 22:32:55 -0500

    Add files via upload

---
+++ project.js
@@ -0,0 +1,29 @@
+function weeklyMeal(){
+
+    var list = ["Breakfast", "MorningSnack","Lunch","EveningSnack","Dinner"];
+    
+    
+
+    var results = document.querySelector(".results");
+    
+        list.forEach ((m,index) => {
+        
+        var meals = document.querySelectorAll("." + m + " .meal");
+        
+        results.innerHTML += m + ": " + "</br>" + "</br>";
+        
+        meals.forEach (p=>{
+            
+            results.innerHTML += p.value + "</br>";
+        }
+                      
+                      );
+        
+        
+        
+        
+        
+        
+    });
+}
+//This project created by:  Pratima Basnet
